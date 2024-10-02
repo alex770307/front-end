248b241e5c1ea5898297663bfc447c334f2d9da2
alex_ <alex_7377@hotmail.com>
2024-10-03 00:10:37 +0200

    9

--- homework10/example05/main.js
+++ homework10/example05/main.js
@@ -5,18 +5,23 @@
 
 // • кратные трём — на fizz; • кратные пяти — на buzz; • кратные и трём, и пяти одновременно — на fizzbuzz.
 
-function fizzBuzz(n = 1) {
+const fizzBuzz = (n = 1) => {
+
     for (let i = 1; i <= n; i++) {
+
         if (i % 3 === 0 && i % 5 === 0) {
             console.log("fizzbuzz");
+
         } else if (i % 3 === 0) {
             console.log("fizz");
+
         } else if (i % 5 === 0) {
             console.log("buzz")
+
         } else {
             console.log(i);
         }
 
     }
 }
-fizzBuzz(25);
+fizzBuzz(35);
