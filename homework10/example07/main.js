0d747a1f800afdc28eca7e9a0e098255ebb7e6c4
alex_ <alex_7377@hotmail.com>
2024-10-03 00:13:07 +0200

    10

--- homework10/example07/main.js
+++ homework10/example07/main.js
@@ -9,7 +9,7 @@
 // console.log(intersection([1, 5, 4, 2], [7, 12])); // должна вернуть [] 
 // console.log(intersection([1, 1, 0, 0, 0, 1, 5, 4, 2], [7, 12, 1, 1, 1, 1, 1, 0, 0, 0, 0])); // должна вернуть []
 
-function intersection(arr1, arr2) {
+const intersection = (arr1, arr2) => {
 
     const array = [];
 
