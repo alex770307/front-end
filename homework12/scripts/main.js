97b901b021a897f5835be251dc44d71d08ba5295
alex_ <alex_7377@hotmail.com>
2024-10-15 16:02:43 +0200

    1

---
+++ homework12/scripts/main.js
@@ -0,0 +1,34 @@
+
+
+const buttonElement = document.querySelector('button');
+const resultElement = document.querySelector('.result');
+const categoryElement = document.querySelector('.category');
+const inputElements = document.querySelectorAll('input');
+
+buttonElement.addEventListener('click', buttonClickHandler);
+
+function buttonClickHandler(){
+   
+    let height = inputElements[0].value;
+    let weight = inputElements[1].value;
+    
+    let bmi = +weight / (+height * +height);
+    resultElement.textContent = bmi;
+    categoryElement.textContent = getBMICategory(bmi);
+}
+
+function getBMICategory(bmi) {
+    if (bmi < 18.5) {
+        return 'Дефицит веса (истощение)';
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        return 'Нормальный вес';
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        return 'Избыточный вес';
+    } else if (bmi >= 30 && bmi <= 34.9) {
+        return 'Ожирение 1 степени';
+    } else if (bmi >= 35 && bmi <= 39.9) {
+        return 'Ожирение 2 степени';
+    } else {
+        return 'Ожирение 3 степени';
+    }
+}
