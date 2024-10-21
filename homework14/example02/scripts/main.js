016cc13918c162043ef4ccc2b21825bbe2a1999e
Admin <1119321.ab@mail.ru>
2024-10-22 00:57:48 +0200

    hw14_4

--- homework14/example02/scripts/main.js
+++ homework14/example02/scripts/main.js
@@ -25,7 +25,9 @@ const links = [
   const ul = document.querySelector('ul');
 
 const createAndAddLinks = (arr) => {
+
   const linksArr = arr.map(
+  
     (link) => {
     const li = document.createElement('li');
     const a = document.createElement('a')
@@ -34,6 +36,7 @@ const createAndAddLinks = (arr) => {
     li.append(a);
     ul.append(li);
     })
+  
   return linksArr;
 }
 
