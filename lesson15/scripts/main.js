b65118c26a287806f7d27e730b5820f95989b5b6
Admin <1119321.ab@mail.ru>
2024-10-23 03:22:52 +0200

    L15

---
+++ lesson15/scripts/main.js
@@ -0,0 +1,43 @@
+
+const buttonAll = document.querySelector("#all");
+const buttonCompleted = document.querySelector("#completed");
+const buttonUncompleted = document.querySelector("#uncompleted");
+const buttonCreate = document.querySelector(".btn");
+const taskInput = document.querySelector(".task-input");
+const list = document.querySelector(".list");
+
+const buttonStatus = document.querySelectorAll(".status");
+
+let arrElements = [];
+
+function addElement() {
+    const obj = {};
+    obj.key = taskInput.value;
+    obj.value = true;
+    const newListItemElement = document.createElement('li');
+    newListItemElement.textContent = obj.key;
+    newListItemElement.classList.add("list-item");
+    list.append(newListItemElement);
+    arrElements.push(obj);
+};
+const array1 = arrElements.forEach(addElement);
+//function                   //которая отрисовывает данные
+
+buttonCreate.addEventListener("click", addElement);
+buttonStatus.addEventListener("click", chooseStatus);
+
+function chooseStatus(buttonAll) {
+    const filteredTasks = array1.filter(
+        (task) => {
+            if (buttonAll === buttonCompleted) return true;
+            if (buttonAll === buttonUncompleted) return false;
+        
+        }
+    )
+   
+};
+
+
+buttonAll.addEventListener("click",  chooseStatus('all'));
+buttonCompleted.addEventListener("click",  chooseStatus('completed'));
+buttonUncompleted.addEventListener("click",  chooseStatus('uncompleted'));
