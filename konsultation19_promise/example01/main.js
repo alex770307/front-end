bbe97b12bb1fa5a8ae636d170d76ae70ac14b8b1
alex_ <alex_7377@hotmail.com>
2024-11-06 20:12:16 +0100

    k_19_3

--- konsultation19_promise/example01/main.js
+++ konsultation19_promise/example01/main.js
@@ -5,21 +5,28 @@ const nameElement = document.querySelector('#name');
 
 buttonElement.addEventListener('click', () => {
     const inputValue = inputElement.value;
-    const getUserInfo = async () => {
-    const response = await fetch(`https://api.github.com/users/${inputValue}`);
-    const userInfo = await response.json();
 
-        updateUserData(userInfo);
+    const getUserInfo = async () => {
+        try {
+            const response = await fetch(`https://api.github.com/users/${inputValue}`);
+            if (!response.ok) throw new Error('User not found');
+            const userInfo = await response.json();
+            updateUserData(userInfo);
+        } catch (error) {
+            console.error('Error:', error);
+        } finally {
+            console.log('Request completed.');
+          }
     }
     getUserInfo(inputValue);
 });
 
 function updateUserData(user) {
     console.log(user);
-   
+
     const imgElement = document.createElement('img');
     imgElement.src = user.avatar_url;
-    avatarElement.innerHTML = ''; 
+    avatarElement.innerHTML = '';
     avatarElement.append(imgElement);
     nameElement.innerHTML = '';
     nameElement.append(user.name);
